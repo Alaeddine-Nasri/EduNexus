9d3c84b6e71b244b44941555041cf40ce1b336b8
Alaeddine-Nasri <ia_nasri@esi.dz>
2023-03-13 16:13:21 +0100

    Init Project BoilePlate

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # EduNexus
+# EduNexus
---
+++ src/components/test/App.tsx
@@ -0,0 +1,11 @@
+function Compo() {
+  return (
+    <div>
+      <h1>Hello From components</h1>
+    </div>
+  );
+}
+
+export default function App() {
+  return <Compo />;
+}
